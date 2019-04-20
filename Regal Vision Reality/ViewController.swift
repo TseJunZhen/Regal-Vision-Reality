fcb3384b839b3fe325a91b8a28a58f114b8fddb7
Tse Victor <victorjunzhen@gmail.com>
2019-04-20 22:31:50 +0800

    Initial Commit

---
+++ Regal Vision Reality/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Regal Vision Reality
+//
+//  Created by TseJunZhen on 20/04/2019.
+//  Copyright © 2019 victorjunzhen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
